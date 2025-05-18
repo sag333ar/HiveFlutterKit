a021e73bd5726716ead1e942f5cc6d0637ec39e1
sagar <sag333ar@gmail.com>
2025-05-18 07:10:20 +0530

    generating single bundle

--- my-web-pack/webpack.config.cjs
+++ my-web-pack/webpack.config.cjs
@@ -1,19 +1,39 @@
 const { DefinePlugin, ProvidePlugin } = require('webpack')
+const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
-  // ...
+  entry: './src/index.js', 
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve('../web', ''),
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        default: false,
+      },
+    },
+    runtimeChunk: false,
+    mergeDuplicateChunks: true,
+    concatenateModules: true
+  },
   resolve: {
-    // ...
     fallback: {
       url: false,
       buffer: require.resolve('buffer/')
     }
   },
   plugins: [
-    // ...
     new DefinePlugin({
       'process.env.NODE_DEBUG': false
     }),
+    new DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production'),
+    }),
+    new webpack.optimize.LimitChunkCountPlugin({
+         maxChunks: 1
+     }),
     new ProvidePlugin({
       Buffer: ['buffer', 'Buffer']
     })
