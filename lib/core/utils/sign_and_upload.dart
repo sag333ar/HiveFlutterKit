3cea7f4f9389b208ccc9ac94c510bc9a38e35a9a
lokesh <lokeshburade007@gmail.com>
2025-06-09 11:28:58 +0530

    missing file added

---
+++ lib/core/utils/sign_and_upload.dart
@@ -0,0 +1,54 @@
+import 'dart:convert';
+import 'dart:typed_data';
+import 'package:hive_flutter_kit/core/hive_flutter_kit_platform_interface.dart';
+import 'package:hive_flutter_kit/core/models/upload_image.dart';
+
+Future<UploadResponse> signAndUploadImage({
+  required HiveFlutterKitPlatform aioha,
+  required String uploadUrlServer,
+  required String fileName,
+  required Uint8List fileBytes,
+}) async {
+  var username = await aioha.getCurrentUser();
+  username = username.replaceAll("\"", "");
+  int timeStamp = DateTime.now().millisecondsSinceEpoch ~/ 1000;
+
+  var object = {
+    "signed_message": {"type": "posting", "app": "ecency.app"},
+    "authors": [username],
+    "timestamp": timeStamp,
+  };
+
+  final resultOfSignature = await aioha.signMessage(
+    jsonEncode(object),
+    'posting',
+  );
+
+  final decodedResult = jsonDecode(resultOfSignature);
+
+  if (decodedResult['success'] == true) {
+    object['signatures'] = [decodedResult['result']];
+    var base64StringOfObject = toBase64(jsonEncode(object));
+
+    // Now call the correct uploadImage (returning a map)
+    final uploadResult = await aioha.uploadImage(
+      imageBytes: fileBytes,
+      fileName: fileName,
+      token: base64StringOfObject,
+      uploadUrlSever: uploadUrlServer,
+    );
+
+    return uploadResult;
+  } else {
+    return UploadResponse(
+        success: false,
+        url: null,
+        message: "Failed to sign image: ${decodedResult['error']}");
+  }
+}
+
+String toBase64(String input) {
+  List<int> bytes = utf8.encode(input);
+  String base64Str = base64Encode(bytes);
+  return base64Str;
+}
