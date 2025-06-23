4ee8745c5ecf217bd01c2d78189ed46a5f378a71
kanchan <kanchanshevkar13@gmail.com>
2025-06-23 18:42:25 +0530

    added trending tags component with getTrendingTags gql query

--- lib/core/common/enum.dart
+++ lib/core/common/enum.dart
@@ -1,4 +1,5 @@
 enum ViewMode { list, grid, large }
+
 enum ThreeSpeakVideoFeedType {
   trending,
   newUploads,
@@ -9,4 +10,5 @@ enum ThreeSpeakVideoFeedType {
   commnuityFeed,
   search,
   myFeed,
-} 
+  trendingTags,
+}
