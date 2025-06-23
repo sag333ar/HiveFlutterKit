3ba32327c172de3ed3660dbcf499693d2e00cdb8
sagar <sag333ar@gmail.com>
2025-06-23 14:27:27 +0530

    removed usage of visibility detector

--- lib/ux/three_speak_ux/widgets/visibility_detector.dart
+++ lib/ux/three_speak_ux/widgets/visibility_detector.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter_kit/core/three_speak_core/models/trending_feed_response.dart';
-import 'package:visibility_detector/visibility_detector.dart';
 
 class VisibilityDetectorListView extends StatefulWidget {
   final List<GQLFeedItem> items;
@@ -19,36 +18,12 @@ class VisibilityDetectorListView extends StatefulWidget {
 
 class _VisibilityDetectorListViewState
     extends State<VisibilityDetectorListView> {
-  final Map<int, bool> _visibilityMap = {};
-
-  @override
-  void dispose() {
-    _visibilityMap.clear();
-    super.dispose();
-  }
-
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: widget.items.length,
       itemBuilder: (context, index) {
-        return VisibilityDetector(
-          key: Key('video-$index'),
-          onVisibilityChanged: (visibilityInfo) {
-            final isVisible = visibilityInfo.visibleFraction > 0.5;
-            if (mounted && _visibilityMap[index] != isVisible) {
-              setState(() {
-                _visibilityMap[index] = isVisible;
-              });
-            }
-          },
-          child: widget.itemBuilder(
-            context,
-            widget.items[index],
-            index,
-            _visibilityMap[index] ?? false,
-          ),
-        );
+        return widget.itemBuilder(context, widget.items[index], index, false);
       },
     );
   }
