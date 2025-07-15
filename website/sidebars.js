fdba520cd36af51d0c1df19ffb9f4ff6768a2fe5
lokesh <lokeshburade007@gmail.com>
2025-07-15 17:52:48 +0530

    Video Upload Documentation updated and CurrentUserAccount Widget Documentation created

--- website/sidebars.js
+++ website/sidebars.js
@@ -77,7 +77,6 @@ const sidebars = {
         "threespeak/video-feed-2",
         "threespeak/video-player",
         "threespeak/video-upload",
-        "threespeak/threespeak-current-user-account",
         "threespeak/current-account",
         "threespeak/trending-tags",
         "threespeak/trendingtags-details",
